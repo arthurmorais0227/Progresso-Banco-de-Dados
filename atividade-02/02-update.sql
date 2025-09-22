7be1aa466e5b45dfd971568393af458584e13f29
Arthur Morais <mlkliso4000@gmail.com>
2025-09-22 10:45:49 -0300

    finaliza os update e começa os delete

--- atividade-02/02-update.sql
+++ atividade-02/02-update.sql
@@ -1,7 +1,16 @@
-UPDATE produtos SET preco = preco * 0.8;
+UPDATE produtos SET preco = preco * 0.8
+WHERE categoria = 'Skin-Care';
 
 SELECT * FROM produtos;
 
 UPDATE produtos SET estoque = estoque + 50;
 
 SELECT * FROM produtos;
+
+UPDATE produtos SET preco = preco * 1.15
+WHERE marca = 'Boticário';
+
+SELECT * FROM produtos;
+
+UPDATE produtos SET preco = preco * 0.9
+WHERE estoque > 100;
---
+++ atividade-02/03-delete.sql
@@ -0,0 +1,2 @@
+DELETE FROM produtos
+WHERE username = 'CheatCodeKing';

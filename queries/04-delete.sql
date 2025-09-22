03152fd8f014b97a21d5708eb71621f3d0d51e20
Arthur Morais <mlkliso4000@gmail.com>
2025-09-22 09:18:16 -0300

    comandos iniciais de delete

---
+++ queries/04-delete.sql
@@ -0,0 +1,12 @@
+DELETE FROM usuarios
+WHERE username = 'CheatCodeKing';
+
+SELECT * FROM usuarios;
+
+DELETE FROM usuarios
+WHERE id = 13;
+
+SELECT * FROM usuarios;
+
+DELETE FROM usuarios
+WHERE pontos < 5000;

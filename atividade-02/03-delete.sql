d958d00848314571b36d7c2012d2a66df71cd210
Arthur <mlkliso4000@gmail.com>
2025-09-29 14:05:02 -0300

    correção com o melhor professor do mundo

--- atividade-02/03-delete.sql
+++ atividade-02/03-delete.sql
@@ -12,3 +12,12 @@ DELETE FROM produtos
 WHERE marca = 'Natura';
 
 SELECT * FROM produtos;
+
+DELETE FROM produtos
+WHERE estoque < 200;
+
+DELETE FROM produtos
+WHERE estoque > 25 AND estoque < 40;
+
+DELETE FROM produtos 
+WHERE estoque BETWEEN 250 AND 400;
